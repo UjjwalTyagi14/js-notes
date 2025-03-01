0eda51934f89399cad7fbc1df5edef1486098f90
Ujjwal Tyagi <ujjwaltyagi671@gmail.com>
2025-03-01 22:14:17 +0530

    add basics folder

---
+++ array.js
@@ -0,0 +1,28 @@
+//question
+// let marks=[85,97,44,37,76,60];
+// let count=0,sum=0;
+// for (let i of marks){
+//   sum+= i;
+//   count++;
+// }
+// console.log(`The average is ${sum/count}`);
+
+
+//question2
+// let price=[250,645,300,900,50];
+// for (let i=0;i<price.length;i++){
+//   let offer=price[i]/10;
+//   price[i]=price[i]-offer;
+// }
+// console.log(`after disount=${price}`);
+
+//,from,.of,.isarray
+// console.log(Array.isArray("ujjwal"));
+// console.log(Array.from("ujjwal"));
+// let num1=100;
+// let num2=200;
+// let num3=300;
+// let c=(Array.of(num1,num2,num3));
+// console.log(c);
+
+
